78881ef8d99cd89677f0019fc428375c108904e7
Code Apprentice <codeguru42@gmail.com>
2018-05-05 12:56:03 -0600

    Semicolon

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ class App extends Component {
   }
 
   handleInput(field, e) {
-    this.setState({[field]: Number(e.target.value)})
+    this.setState({[field]: Number(e.target.value)});
   }
 
   submit(e) {
