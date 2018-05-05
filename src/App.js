8bb8ce5cb3b83937e2261320b53edc005ab93846
Code Apprentice <codeguru42@gmail.com>
2018-05-05 11:11:04 -0600

    Add more input controls

--- src/App.js
+++ src/App.js
@@ -20,12 +20,37 @@ class App extends Component {
   render() {
     return (
         <div className="App">
-          <FormGroup controlId={'formName'}>
-            <ControlLabel>Name:</ControlLabel>
+          <FormGroup controlId={'principle'}>
+            <ControlLabel>Principle:</ControlLabel>
             <FormControl type={'text'}
-                         value={this.state.name}
-                         placeholder={'Enter your name'}
-                         onChange={this.handleInput.bind(this, 'name')}
+                         value={this.state.principle}
+                         placeholder={'Principle'}
+                         onChange={this.handleInput.bind(this, 'principle')}
+            />
+          </FormGroup>
+          <FormGroup controlId={'apr'}>
+            <ControlLabel>Interest Rate:</ControlLabel>
+            <FormControl type={'text'}
+                         value={this.state.apr}
+                         placeholder={'APR'}
+                         onChange={this.handleInput.bind(this, 'apr')}
+            />
+          </FormGroup>
+          <FormGroup controlId={'term'}>
+            <ControlLabel>Loan Term:</ControlLabel>
+            <FormControl type={'text'}
+                         value={this.state.term}
+                         placeholder={'Term'}
+                         onChange={this.handleInput.bind(this, 'term')}
+            />
+          </FormGroup>
+          <FormGroup controlId={'payment'}>
+            <ControlLabel>Monthly Payment</ControlLabel>
+            <FormControl type={'text'}
+                         value={this.state.payment}
+                         placeholder={'Monthly Payment'}
+                         onChange={this.handleInput.bind(this, 'payment')}
+                         readonly
             />
           </FormGroup>
           <Button bsStyle={'primary'}
