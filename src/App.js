7543d8b0337e9afcebf48a933450bbc2e3dd003f
Code Apprentice <codeguru42@gmail.com>
2018-05-05 11:48:59 -0600

    2 space indent

--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ class App extends Component {
     super(props, context);
     this.submit = this.submit.bind(this);
     this.state = {
-        principle: '',
-        apr: '',
-        term: '',
-        payment: '',
+      principle: '',
+      apr: '',
+      term: '',
+      payment: '',
     };
   }
 
@@ -23,46 +23,46 @@ class App extends Component {
 
   render() {
     return (
-        <div className="App">
-          <FormGroup controlId={'principle'}>
-            <ControlLabel>Principle:</ControlLabel>
-            <FormControl type={'text'}
-                         value={this.state.principle}
-                         placeholder={'Principle'}
-                         onChange={this.handleInput.bind(this, 'principle')}
-            />
-          </FormGroup>
-          <FormGroup controlId={'apr'}>
-            <ControlLabel>Interest Rate:</ControlLabel>
-            <FormControl type={'text'}
-                         value={this.state.apr}
-                         placeholder={'APR'}
-                         onChange={this.handleInput.bind(this, 'apr')}
-            />
-          </FormGroup>
-          <FormGroup controlId={'term'}>
-            <ControlLabel>Loan Term:</ControlLabel>
-            <FormControl type={'text'}
-                         value={this.state.term}
-                         placeholder={'Term'}
-                         onChange={this.handleInput.bind(this, 'term')}
-            />
-          </FormGroup>
-          <FormGroup controlId={'payment'}>
-            <ControlLabel>Monthly Payment</ControlLabel>
-            <FormControl type={'text'}
-                         value={this.state.payment}
-                         placeholder={'Monthly Payment'}
-                         onChange={this.handleInput.bind(this, 'payment')}
-                         readonly
-            />
-          </FormGroup>
-          <Button bsStyle={'primary'}
-                  onClick={this.submit}
-          >
-            Submit
-          </Button>
-        </div>
+      <div className="App">
+        <FormGroup controlId={'principle'}>
+          <ControlLabel>Principle:</ControlLabel>
+          <FormControl type={'text'}
+                       value={this.state.principle}
+                       placeholder={'Principle'}
+                       onChange={this.handleInput.bind(this, 'principle')}
+          />
+        </FormGroup>
+        <FormGroup controlId={'apr'}>
+          <ControlLabel>Interest Rate:</ControlLabel>
+          <FormControl type={'text'}
+                       value={this.state.apr}
+                       placeholder={'APR'}
+                       onChange={this.handleInput.bind(this, 'apr')}
+          />
+        </FormGroup>
+        <FormGroup controlId={'term'}>
+          <ControlLabel>Loan Term:</ControlLabel>
+          <FormControl type={'text'}
+                       value={this.state.term}
+                       placeholder={'Term'}
+                       onChange={this.handleInput.bind(this, 'term')}
+          />
+        </FormGroup>
+        <FormGroup controlId={'payment'}>
+          <ControlLabel>Monthly Payment</ControlLabel>
+          <FormControl type={'text'}
+                       value={this.state.payment}
+                       placeholder={'Monthly Payment'}
+                       onChange={this.handleInput.bind(this, 'payment')}
+                       readonly
+          />
+        </FormGroup>
+        <Button bsStyle={'primary'}
+                onClick={this.submit}
+        >
+          Submit
+        </Button>
+      </div>
     );
   }
 }
