44c81d4b36ca7e73654a495ee770415e8b6233a8
Code Apprentice <codeguru42@gmail.com>
2018-05-05 12:06:45 -0600

    Use bootstrap's Panel component for layout

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React, {Component} from 'react';
 import './App.css';
-import {Button, ControlLabel, FormControl, FormGroup} from "react-bootstrap";
+import {
+  Button,
+  ControlLabel,
+  FormControl,
+  FormGroup,
+  Panel
+} from "react-bootstrap";
 
 class App extends Component {
   constructor(props, context) {
@@ -23,46 +29,53 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <FormGroup controlId={'principle'}>
-          <ControlLabel>Principle:</ControlLabel>
-          <FormControl type={'text'}
-                       value={this.state.principle}
-                       placeholder={'Principle'}
-                       onChange={this.handleInput.bind(this, 'principle')}
-          />
-        </FormGroup>
-        <FormGroup controlId={'apr'}>
-          <ControlLabel>Interest Rate:</ControlLabel>
-          <FormControl type={'text'}
-                       value={this.state.apr}
-                       placeholder={'APR'}
-                       onChange={this.handleInput.bind(this, 'apr')}
-          />
-        </FormGroup>
-        <FormGroup controlId={'term'}>
-          <ControlLabel>Loan Term:</ControlLabel>
-          <FormControl type={'text'}
-                       value={this.state.term}
-                       placeholder={'Term'}
-                       onChange={this.handleInput.bind(this, 'term')}
-          />
-        </FormGroup>
-        <FormGroup controlId={'payment'}>
-          <ControlLabel>Monthly Payment</ControlLabel>
-          <FormControl type={'text'}
-                       value={this.state.payment}
-                       placeholder={'Monthly Payment'}
-                       onChange={this.handleInput.bind(this, 'payment')}
-                       readonly
-          />
-        </FormGroup>
-        <Button bsStyle={'primary'}
-                onClick={this.submit}
-        >
-          Submit
-        </Button>
-      </div>
+      <Panel className='App'
+             bsStyle='primary'
+      >
+        <Panel.Heading>
+          <Panel.Title>Loan Calculator</Panel.Title>
+        </Panel.Heading>
+        <Panel.Body>
+          <FormGroup controlId={'principle'}>
+            <ControlLabel>Principle:</ControlLabel>
+            <FormControl type={'text'}
+                         value={this.state.principle}
+                         placeholder={'Principle'}
+                         onChange={this.handleInput.bind(this, 'principle')}
+            />
+          </FormGroup>
+          <FormGroup controlId={'apr'}>
+            <ControlLabel>Interest Rate:</ControlLabel>
+            <FormControl type={'text'}
+                         value={this.state.apr}
+                         placeholder={'APR'}
+                         onChange={this.handleInput.bind(this, 'apr')}
+            />
+          </FormGroup>
+          <FormGroup controlId={'term'}>
+            <ControlLabel>Loan Term:</ControlLabel>
+            <FormControl type={'text'}
+                         value={this.state.term}
+                         placeholder={'Term'}
+                         onChange={this.handleInput.bind(this, 'term')}
+            />
+          </FormGroup>
+          <FormGroup controlId={'payment'}>
+            <ControlLabel>Monthly Payment</ControlLabel>
+            <FormControl type={'text'}
+                         value={this.state.payment}
+                         placeholder={'Monthly Payment'}
+                         onChange={this.handleInput.bind(this, 'payment')}
+                         readonly
+            />
+          </FormGroup>
+          <Button bsStyle={'primary'}
+                  onClick={this.submit}
+          >
+            Submit
+          </Button>
+        </Panel.Body>
+      </Panel>
     );
   }
 }
