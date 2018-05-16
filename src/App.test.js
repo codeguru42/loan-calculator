acf33b8eb212fae80096d53f4e002f2dc0f72bf8
Code Apprentice <codeguru42@gmail.com>
2018-05-15 20:36:49 -0600

    Add another test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import TestRenderer from 'react-test-renderer';
+import {Panel} from "react-bootstrap";
 import App from './App';
 
 it('renders without crashing', () => {
   const testRenderer = TestRenderer.create(<App />);
+  testRenderer.unmount();
+});
+
+it('renders a Panel control', () => {
+  const testRenderer = TestRenderer.create(<App />);
+  const app = testRenderer.root;
+  const formGroups = app.findAllByType(Panel);
+  expect(formGroups).toHaveLength(1);
   testRenderer.unmount();
 });
