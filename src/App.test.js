a403b06d0de6ee7244552f8c9b83f3578a2cd73f
Code Apprentice <codeguru42@gmail.com>
2018-05-15 20:28:55 -0600

    Implement test with react-test-renderer

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import TestRenderer from 'react-test-renderer';
 import App from './App';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  const testRenderer = TestRenderer.create(<App />);
+  testRenderer.unmount();
 });
